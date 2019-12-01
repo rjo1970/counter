6d8355e7535b079f129daaa6337838cdd77385e4
RJ Osborne <rjo1970@gmail.com>
2019-12-01 08:12:33 -0500

    Cleaned up tests.

--- test/counter_test.exs
+++ test/counter_test.exs
@@ -1,21 +1,32 @@
 defmodule CounterTest do
   use ExUnit.Case
 
-  test "It can read" do
+  setup context do
     Counter.reset()
+
+    context
+  end
+
+  test "It can read" do
     assert Counter.read() == 0
   end
 
   test "It can increment" do
-    Counter.reset()
     Counter.inc()
     assert Counter.read() == 1
   end
 
   test "It can reset" do
-    0..500 |> Enum.each(fn _x -> Counter.inc() end)
+    1..5 |> Enum.each(fn _x -> Counter.inc() end)
     Counter.reset()
     assert Counter.read() == 0
+  end
+
+  test "It continues to increment after read" do
+    1..5 |> Enum.each(fn _x -> Counter.inc() end)
+    Counter.read()
+    1..5 |> Enum.each(fn _x -> Counter.inc() end)
+    assert Counter.read() == 10
   end
 
   test "It can reset to arbitrary values" do
