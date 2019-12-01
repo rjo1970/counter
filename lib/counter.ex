83981b786e609c8c164a6b5475d3b7210a46def2
RJ Osborne <rjo1970@gmail.com>
2019-12-01 07:17:50 -0500

    Added a default of 0 to reset.

--- lib/counter.ex
+++ lib/counter.ex
@@ -1,5 +1,5 @@
 defmodule Counter do
   defdelegate inc(), to: Counter.Api
   defdelegate read(), to: Counter.Api
-  defdelegate reset(value), to: Counter.Api
+  defdelegate reset(value \\ 0), to: Counter.Api
 end
--- test/counter_test.exs
+++ test/counter_test.exs
@@ -2,19 +2,24 @@ defmodule CounterTest do
   use ExUnit.Case
 
   test "It can read" do
-    Counter.reset(0)
+    Counter.reset()
     assert Counter.read() == 0
   end
 
   test "It can increment" do
-    Counter.reset(0)
+    Counter.reset()
     Counter.inc()
     assert Counter.read() == 1
   end
 
   test "It can reset" do
     0..500 |> Enum.each(fn _x -> Counter.inc() end)
-    Counter.reset(0)
+    Counter.reset()
     assert Counter.read() == 0
+  end
+
+  test "It can reset to arbitrary values" do
+    Counter.reset(123)
+    assert Counter.read() == 123
   end
 end
